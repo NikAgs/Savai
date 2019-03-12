42e55761d4918225d2e62045713f754e6aec876a
NikAgs <thenikiscooking@gmail.com>
2019-03-11 23:18:05 -0700

    Updated emailing backend

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -91,7 +91,7 @@ if ($('.mailchimp').length>0) {
     $('.mailchimp').ajaxChimp({
         language: 'es',
         callback: mailchimpCallback,
-        url: "//mixdesigns.us4.list-manage.com/subscribe/post?u=d21d287a2a3620961a7419c49&amp;id=a4d425b5b6" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".
+        url: "https://savai.us20.list-manage.com/subscribe/post?u=5c4ce45b8717d78b8e252fd56&amp;id=83502ba094" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".
     });
 }
     
@@ -119,8 +119,10 @@ $(document).on('submit', '#contactForm', function (e) {
     e.preventDefault();
     var name = $("#name").val();
     var email = $("#email").val();
+    var phone = $("#phone").val();
+    var business = $("#business").val();
     var message = $("#message").val();
-    var dataString = 'name=' + name + '&email=' + email + '&message=' + message;
+    var dataString = 'name=' + name + '&email=' + email + '&message=' + message + '&phone=' + phone + '&business=' + business;
     function isValidEmail(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
--- HTML/sendmail.php
+++ HTML/sendmail.php
@@ -1,7 +1,7 @@
 <?php
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
-if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+if ( isset($_POST['phone']) && isset($_POST['business']) && isset($_POST['email']) && isset($_POST['name']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
   // detect & prevent header injections
   $test = "/(content-type|bcc:|cc:|to:)/i";
@@ -11,10 +11,6 @@ if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['message'])
     }
   }
 
-  //
-  mail( "sarina@savai.co", WAVA , $_POST['message'], "From:" . $_POST['email'] );
-
-  //			^
-  //  Replace with your email
+  mail( "sarina@savai.co", "New message!" , "Name: " . $_POST['name'] . "\nBusiness: " . $_POST['business'] . "\nPhone: " . $_POST['phone'] . "\n" . $_POST['message'], "From:" . $_POST['email'] );
 }
 ?>
